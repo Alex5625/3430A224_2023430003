6945903751032fae027496fe1b6c024866a85798
Alex5625 <ahernandez23@alumnos.utalca.cl>
2024-08-27 09:32:08 -0400

    se modifica el tipo de variable altura

--- paciente_estructura.cpp
+++ paciente_estructura.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-// Define estructura de paciente, el peso es en kg, altura en metros
+// Define estructura de paciente, el peso es en kg, altura en centimetros
 struct Paciente {
     string nombre;
     int edad;
-    double altura;
+    int altura;
     double peso;
 };
 
@@ -19,19 +19,19 @@ int main(){
     Paciente paciente1;
     paciente1.nombre = "ola";
     paciente1.edad = 23;
-    paciente1.altura = 1.67;
+    paciente1.altura = 167;
     paciente1.peso = 45.8;
 
     Paciente paciente2;
     paciente2.nombre = "Amarlo";
     paciente2.edad = 54;
-    paciente2.altura = 1.54;
+    paciente2.altura = 154;
     paciente2.peso = 39.1;
 
     Paciente paciente3;
     paciente3.nombre = "Erlo";
     paciente3.edad = 12;
-    paciente3.altura = 1.21;
+    paciente3.altura = 121;
     paciente3.peso = 32.1;
 
 
@@ -42,14 +42,14 @@ int main(){
 
     cout << paciente2.nombre << "\n";
     cout << paciente2.edad << "\n" ;
-    cout << paciente3.altura << "\n";
+    cout << paciente2.altura << "\n";
     cout << paciente2.peso << "\n\n" ;
 
     cout << paciente3.nombre << "\n" ;
-    cout << paciente3.altura << "\n";
     cout << paciente3.edad << "\n" ;
+    cout << paciente3.altura << "\n";
     cout << paciente3.peso << "\n\n";
 
-
+    return 0;
 
 }
