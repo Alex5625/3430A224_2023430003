4b02eae75ab7010818840f149430ae7bb622eff0
Alex5625 <ahernandez23@alumnos.utalca.cl>
2024-10-18 14:59:21 -0300

    se finaliza el codigo de ordenamiento

--- lab8alexisHernandez.cpp
+++ lab8alexisHernandez.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <cstdlib> // Para rand() y srand()
 #include <ctime>   // Para time()
-#include <chrono> //para chrono
+#include <chrono> // Para chrono()
+#include <cctype> // Para tolower()
 using namespace std;
+
+
 void llenarArreglo(int* arr, int size) {
     // Llenar el arreglo con numeros aleatorios entre 1 y 100
     for (int i = 0; i < size; ++i) {
@@ -13,7 +16,7 @@ void llenarArreglo(int* arr, int size) {
 void mostrarArreglo(int* arr, int size) {
     // Mostrar los valores del arreglo
     for (int i = 0; i < size; ++i) {
-        cout << arr[i] << " ";
+        cout << "arr[" << i << "]" << "= " << arr[i] << " ";
     }
     cout << endl;
 }
@@ -70,10 +73,10 @@ int main(int argc, char **argv) {
         cout << "Por favor, ingrese los 2 parametros correspondientes como argumento.\n";
         return 1;
     }
-    const int SIZE = atoi(argv[1]); // TAMANO DEL ARREGLO
+    const double SIZE = atod(argv[1]); // TAMANO DEL ARREGLO
 
     char caracter = argv[2][0]; // VERIFICACION DE MOSTRAR LOS ARREGLOS O NO
-    if (caracter != 'n' || caracter != 's'){
+    if (caracter != 'n' && caracter != 's'){
         cout << "No añadiste uno de los parametros para mostrar el arreglo\nUSA -n- PARA NO MOSTRAR EL ARREGLO Y -s- PARA MOSTRAR EL ARREGLO\n " << endl;
         return 1;
     }
@@ -86,35 +89,53 @@ int main(int argc, char **argv) {
     // Funciones para llenar e imprimir el arreglo
     llenarArreglo(arr1, SIZE);
 
-    cout << "Arreglo de números enteros (antes de SelectionSort):\n";
-    mostrarArreglo(arr1, SIZE);
+    if (caracter == 's'){
+        cout << "--------------------------------------\n";
+        cout << "\nArreglo antes del SelectionSort:\n";
+        mostrarArreglo(arr1, SIZE);        
+        cout << "--------------------------------------\n";
+    }
 
     // Mide el tiempo de ejecución de SelectionSort
     auto start = chrono::high_resolution_clock::now();
     seleccion(arr1, SIZE);
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double, milli> duration = end - start; // Milisegundos
+    cout << "///////////////////////////////////////\n";
     cout << "Duración del método SelectionSort: " << duration.count() << " ms\n";
+    cout << "///////////////////////////////////////\n";
 
-    cout << "Arreglo después de SelectionSort:\n";
-    mostrarArreglo(arr1, SIZE);
+    if (caracter == 's'){
+        cout << "*************************************\n";
+        cout << "Arreglo después de SelectionSort:\n";
+        mostrarArreglo(arr1, SIZE);
+        cout << "*************************************\n";
+    }
 
     //----------- QuickSort ------------------------
     int arr2[SIZE];
     llenarArreglo(arr2, SIZE);
 
-    cout << "\nArreglo de números enteros (antes de QuickSort):\n";
-    mostrarArreglo(arr2, SIZE);
+    if (caracter == 's'){        
+        cout << "--------------------------------------\n";
+        cout << "\nArreglo antes del QuickSort:\n";
+        mostrarArreglo(arr2, SIZE);
+        cout << "--------------------------------------\n";
+    }
 
     // Mide el tiempo de ejecución de QuickSort
     start = chrono::high_resolution_clock::now();
-    QuickSort(arr2, 0, SIZE - 1); // Corrección aquí
+    QuickSort(arr2, 0, SIZE - 1); 
     end = chrono::high_resolution_clock::now();
     duration = end - start;
+    cout << "///////////////////////////////////////\n";
     cout << "Duración del método QuickSort: " << duration.count() << " ms\n";
-
-    cout << "Arreglo después de QuickSort:\n";
-    mostrarArreglo(arr2, SIZE);
-
+    cout << "///////////////////////////////////////\n";
+    if (caracter == 's'){
+        cout << "*************************************\n";
+        cout << "Arreglo después de QuickSort:\n";
+        mostrarArreglo(arr2, SIZE);
+        cout << "*************************************\n";
+    }
     return 0;
 }
